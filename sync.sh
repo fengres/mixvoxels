88b4ce650e3d088b387e1a615484b17e20ef56f5
fengres <wangfeng_work2020@163.com>
2022-12-01 04:29:36 +0800

    init-pages

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-pass=lxh970812
-expect -c "
-    spawn rsync -av ./ lxh@166.111.131.98:/data/machine/mixvoxels_pub/
-    expect {
-            *assword {set timeout 300; send $pass\r; exp_continue;}
-    }
-    exit
-    "
